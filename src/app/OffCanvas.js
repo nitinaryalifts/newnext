736dd3df7865aca9f58c76f851b39045cf67305b
nitinaryalifts <nitinaryabrawny@gmail.com>
2024-10-08 11:53:06 +0530

    deployed

--- src/app/OffCanvas.js
+++ src/app/OffCanvas.js
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useRef, useState } from 'react';
 import Payment from './CheckoutForm'; // Make sure this path is correct
 import { FaCartPlus } from "react-icons/fa";
--- src/app/PaymentModal.js
+++ src/app/PaymentModal.js
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState } from 'react';
 import { DoseProvider } from './DoseContext';
 import Payment from './CheckoutForm';
